f1bd2dfd44b2ebb94d764c0b7e6f069b40503982
Oliver Ruebel <oruebel@lbl.gov>
2022-04-13 21:43:28 -0700

    Updated to match new HDMF approach to permitting zarr arrays. Renamed lib back to hdmf_zarr

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ autodoc_member_order = 'bysource'
 
 # -- Project information -----------------------------------------------------
 
-project = 'hdmf_zarr_plugin'
+project = 'hdmf_zarr'
 copyright = '2022, Oliver Ruebel'
 author = 'Oliver Ruebel'
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,7 +10,7 @@ Welcome to hdmf-zarr's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-   hdmf_zarr_plugin
+   hdmf_zarr
 
 
 
