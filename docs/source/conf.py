67ac984311e908b1f0605c2b1612df1e2920d983
Oliver Ruebel <oruebel@lbl.gov>
2022-04-14 04:12:52 -0700

    Added logo for hdmf-zarr

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,6 +109,20 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'figures/logo_hdmf_zarr.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+# html_favicon = 'figures/logo_hdmf_zarr_iconsize.png'
+
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+latex_logo = 'figures/logo_hdmf_zarr.pdf'
+
 
 # -- sphinx extension ----------------------------------------------------
 
