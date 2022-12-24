8ebb6e4341e04e84ed00b20a28efe1f5d7bef793
Oliver Ruebel <oruebel@lbl.gov>
2022-12-24 03:46:03 -0800

    Fix bug with wrong number of electrodes used in ElectricalSeries in the NWB tutorial

--- docs/gallery/plot_nwb_zarrio.py
+++ docs/gallery/plot_nwb_zarrio.py
@@ -84,7 +84,7 @@ all_table_region = nwbfile.create_electrode_table_region(
 )
 
 # Add a mock electrical recording acquisition to the NWBFile
-raw_data = np.random.randn(50, 4)
+raw_data = np.random.randn(50, len(all_table_region))
 raw_electrical_series = ElectricalSeries(
     name="ElectricalSeries",
     data=raw_data,
@@ -95,7 +95,7 @@ raw_electrical_series = ElectricalSeries(
 nwbfile.add_acquisition(raw_electrical_series)
 
 # Add a mock LFP processing result to the NWBFile
-lfp_data = np.random.randn(50, 4)
+lfp_data = np.random.randn(50, len(all_table_region))
 lfp_electrical_series = ElectricalSeries(
     name="ElectricalSeries",
     data=lfp_data,
