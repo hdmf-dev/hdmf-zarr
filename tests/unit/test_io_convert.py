6a6b8f0a5587f65e07ac533df6bbcca4c8f62854
Oliver Ruebel <oruebel@lbl.gov>
2023-01-07 01:27:42 -0800

    Update test_io_convert.py to test with all supported zarr.storage backends

--- tests/unit/test_io_convert.py
+++ tests/unit/test_io_convert.py
@@ -2,19 +2,28 @@
 Module for testing conversion of data between different I/O backends
 To reduce the amount of code needed, the tests use a series of mixin classes to
 construct a test case:
-- MixinTestCaseConvert is the base mixin class for conversion tests and
-  requires that the setUpContainer and roundtripExportContainer functions
+
+- ``MixinTestCaseConvert`` is the base mixin class for conversion tests and
+  requires that the ``setUpContainer`` and ``roundtripExportContainer`` functions
   are defined. The setUpContainer defines the container (and hence the problem case)
-  to be written to file. And the rountripExportContainer defined the process
+  to be written to file. And the roundtripExportContainer defined the process
   for writing, exporting, and then reading the container again.
-- TestXYZContainerMixin classes define the setUpContainer function
-- TestX1toX2Mixin defines the rountripExportContainer process
-- TestCase is the base test class for HDMF
+- ``TestXYZContainerMixin`` classes define the ``setUpContainer`` function
+- ``TestX1toX2Mixin`` defines the ``roundtripExportContainer`` process
+- ``TestCase`` is the base test class for HDMF
+
 A test case is then constructed by defining a class that inherits from the
-corresponding (usually 4) base classes, a mixin that define setUpContainer,
-a mixin that define roundtripExportContainer, MixinTestCaseConvert, and TestCase.
+corresponding (usually 4) base classes:
+
+1. a mixin that define ``setUpContainer``,
+2. a mixin that define ``roundtripExportContainer``,
+3. ``MixinTestCaseConvert``, and
+4. TestCase.
+
 I.e., even though a particular test class may look empty, it is the combination
-of the mixin classes that creates the particular test problem.
+of the mixin classes that creates the particular test problem. Many of the Mixin
+classes also define additional class variables to allow child classes to further
+customize the behavior of the mixin.
 """
 import os
 import shutil
@@ -34,28 +43,58 @@ from tests.unit.utils import (Foo,
                               FooFile,
                               get_foo_buildmanager)
 
+from zarr.storage import (DirectoryStore,
+                          TempStore,
+                          NestedDirectoryStore)
+
 
 class MixinTestCaseConvert(metaclass=ABCMeta):
     """
     Mixin class used to define the basic structure for a conversion test.
+
+    To implement a test case using this mixin we need to implement the abstract methods
+    ``setUpContainer`` and ``roundtripExportContainer``. The behavior of the mixin can
+    then be further customized via the following variables:
+
+    :ivar IGNORE_NAME: Bool parameter passed to assertContainerEqual (False)
+    :ivar IGNORE_HDMF_ATTRS = Bool parameter passed to assertContainerEqual (False)
+    :ivar IGNORE_STRING_TO_BYTE = Bool parameter passed to assertContainerEqual (False)
+    :ivar WRITE_PATHS = List of paths to which to write files to as part of ``test_export_roundtrip``,
+                        which passes the values to ``roundtripContainer``. The specific definition
+                        of the individual paths depends on the backend used for writing in ``rountripContainer``.
+                        E.g., if :py:class:`~hdmf.backends.h5tools.HDF5IO` is used then the paths must be strings,
+                        and when :py:class:`~hdmf_zarr.backend.ZarrIO` is used then paths may be strings or
+                        supported ``zarr.storage`` backend objects, e.g., a ``zarr.storage.DirectoryStore``.
+                        A value of None as part of list means to use the default filename for write.
+                        (Default=[None, ])
+    :ivar EXPORT_PATHS = List of paths to which to export files to as part of ``test_export_roundtrip``,
+                        which passes the values to ``roundtripContainer``. The specific definition
+                        of the individual paths depends on the backend used for writing in ``rountripContainer``.
+                        E.g., if :py:class:`~hdmf.backends.h5tools.HDF5IO` is used then the paths must be strings,
+                        and when :py:class:`~hdmf_zarr.backend.ZarrIO` is used then paths may be strings or
+                        supported ``zarr.storage`` backend objects, e.g., a ``zarr.storage.DirectoryStore``.
+                        A value of None as part of list means to use the default filename for export.
+                        (Default=[None, ])
+
     """
     IGNORE_NAME = False
     IGNORE_HDMF_ATTRS = False
     IGNORE_STRING_TO_BYTE = False
+    WRITE_PATHS = [None, ]
+    EXPORT_PATHS = [None, ]
 
     def get_manager(self):
         raise NotImplementedError('Cannot run test unless get_manger  is implemented')
 
     def setUp(self):
         self.__manager = self.get_manager()
-        self.container = self.setUpContainer()
-        self.container_type = self.container.__class__.__name__
-        self.filename = 'test_%s.hdmf' % self.container_type
-        self.export_filename = 'test_export_%s.hdmf' % self.container_type
-        self.filenames = [self.filename, self.export_filename]
+        self.filenames = []
         self.ios = []
 
     def tearDown(self):
+        self.close_files_and_ios()
+
+    def close_files_and_ios(self):
         for io in self.ios:
             if io is not None:
                 io.close()
@@ -65,6 +104,8 @@ class MixinTestCaseConvert(metaclass=ABCMeta):
                     shutil.rmtree(fn)
                 else:
                     os.remove(fn)
+        self.filenames = []
+        self.ios = []
 
     @abstractmethod
     def setUpContainer(self):
@@ -72,31 +113,51 @@ class MixinTestCaseConvert(metaclass=ABCMeta):
         raise NotImplementedError('Cannot run test unless setUpContainer is implemented')
 
     @abstractmethod
-    def roundtripExportContainer(self):
+    def roundtripExportContainer(self, container, write_path, export_path):
         """
-        1. Write the container to self.filename
-        2. Export the file from 1 to self.export_filename using a new backend
-        3. Read the exported container from disk
-        4. Return the container read in 4 so that it can be compared with the original
-        Any HDMFIO backends that should remain open should be added to the self.io list
-        so that they can be closed on tearDown.
+        1. Write the container to write_path
+        2. Export the file from write_path to export_path using a new backend
+        3. Read the exported container export_path from disk
+        4. Return the container read in 3 so that it can be compared with the original
+        Any HDMFIO backends that should remain open MUST be added to the self.io list
+        so that they can be closed by close_files_and_ios (e.g., on tearDown),
         """
         raise NotImplementedError('Cannot run test unless roundtripExportContainer  is implemented')
 
     def test_export_roundtrip(self):
         """Test that rountripping the container works"""
-        exported_container = self.roundtripExportContainer()
-        self.assertIsNotNone(str(self.container))  # added as a test to make sure printing works
-        self.assertIsNotNone(str(exported_container))
-        # make sure we get a completely new object
-        self.assertNotEqual(id(self.container), id(exported_container))
-        # the name of the root container of a file is always 'root' (see h5tools.py ROOT_NAME)
-        # thus, ignore the name of the container when comparing original container vs read container
-        self.assertContainerEqual(self.container, exported_container,
-                                  ignore_name=self.IGNORE_NAME,
-                                  ignore_hdmf_attrs=self.IGNORE_HDMF_ATTRS,
-                                  ignore_string_to_byte=self.IGNORE_STRING_TO_BYTE)
-        # TODO May need to add further asserts here
+        # determine and save the write and export paths
+        for write_path in self.WRITE_PATHS:
+            for export_path in self.EXPORT_PATHS:
+                container = self.setUpContainer()
+                container_type = container.__class__.__name__
+                if write_path is None:
+                    write_path = 'test_%s.hdmf' % container_type
+                if export_path is None:
+                    export_path = 'test_export_%s.hdmf' % container_type
+                self.filenames.append(write_path if isinstance(write_path, str) else write_path.path)
+                self.filenames.append(export_path if isinstance(export_path, str) else export_path.path)
+                # roundtrip the container
+                exported_container = self.roundtripExportContainer(
+                    container=container,
+                    write_path=write_path,
+                    export_path=export_path)
+                # assert that the rountrip worked correctly
+                message = "Using: write_path=%s, export_path=%s" % (str(write_path), str(export_path))
+                self.assertIsNotNone(str(container), message)  # added as a test to make sure printing works
+                self.assertIsNotNone(str(exported_container), message)
+                # make sure we get a completely new object
+                self.assertNotEqual(id(container), id(exported_container), message)
+                # the name of the root container of a file is always 'root' (see h5tools.py ROOT_NAME)
+                # thus, ignore the name of the container when comparing original container vs read container
+                self.assertContainerEqual(container,
+                                          exported_container,
+                                          ignore_name=self.IGNORE_NAME,
+                                          ignore_hdmf_attrs=self.IGNORE_HDMF_ATTRS,
+                                          ignore_string_to_byte=self.IGNORE_STRING_TO_BYTE)
+                # TODO: Add message to the assertContainerEqual call once the feature is released in HDMF
+                self.close_files_and_ios()
+                # TODO: May need to add further asserts here
 
 
 ############################################
@@ -109,18 +170,24 @@ class MixinTestHDF5ToZarr():
     The setUpContainer function required for the test needs to be defined separately
     (e.g., by another mixin or the test class itself).
     """
+    WRITE_PATHS = [None, ]
+    EXPORT_PATHS = [None,
+                    DirectoryStore('test_export_DirectoryStore.zarr'),
+                    TempStore(),
+                    NestedDirectoryStore('test_export_NestedDirectoryStore.zarr')]
+
     def get_manager(self):
         return get_hdmfcommon_manager()
 
-    def roundtripExportContainer(self):
-        with HDF5IO(self.filename, manager=self.get_manager(), mode='w') as write_io:
-            write_io.write(self.container, cache_spec=True)
+    def roundtripExportContainer(self, container, write_path, export_path):
+        with HDF5IO(write_path, manager=self.get_manager(), mode='w') as write_io:
+            write_io.write(container, cache_spec=True)
 
-        with HDF5IO(self.filename, manager=self.get_manager(), mode='r') as read_io:
-            with ZarrIO(self.export_filename, mode='w') as export_io:
+        with HDF5IO(write_path, manager=self.get_manager(), mode='r') as read_io:
+            with ZarrIO(export_path, mode='w') as export_io:
                 export_io.export(src_io=read_io, write_args={'link_data': False})
 
-        read_io = ZarrIO(self.export_filename, manager=self.get_manager(), mode='r')
+        read_io = ZarrIO(export_path, manager=self.get_manager(), mode='r')
         self.ios.append(read_io)
         exportContainer = read_io.read()
         return exportContainer
@@ -133,18 +200,24 @@ class MixinTestZarrToHDF5():
     The setUpContainer function required for the test needs to be defined separately
     (e.g., by another mixin or the test class itself)
     """
+    WRITE_PATHS = [None,
+                   DirectoryStore('test_export_DirectoryStore.zarr'),
+                   TempStore(),
+                   NestedDirectoryStore('test_export_NestedDirectoryStore.zarr')]
+    EXPORT_PATHS = [None, ]
+
     def get_manager(self):
         return get_hdmfcommon_manager()
 
-    def roundtripExportContainer(self):
-        with ZarrIO(self.filename, manager=self.get_manager(), mode='w') as write_io:
-            write_io.write(self.container, cache_spec=True)
+    def roundtripExportContainer(self, container,  write_path, export_path):
+        with ZarrIO(write_path, manager=self.get_manager(), mode='w') as write_io:
+            write_io.write(container, cache_spec=True)
 
-        with ZarrIO(self.filename, manager=self.get_manager(), mode='r') as read_io:
-            with HDF5IO(self.export_filename, mode='w') as export_io:
+        with ZarrIO(write_path, manager=self.get_manager(), mode='r') as read_io:
+            with HDF5IO(export_path,  mode='w') as export_io:
                 export_io.export(src_io=read_io, write_args={'link_data': False})
 
-        read_io = HDF5IO(self.export_filename, manager=self.get_manager(), mode='r')
+        read_io = HDF5IO(export_path, manager=self.get_manager(), mode='r')
         self.ios.append(read_io)
         exportContainer = read_io.read()
         return exportContainer
@@ -156,10 +229,11 @@ class MixinTestDynamicTableContainer():
     test export of DynamicTable container classes. This class only defines the setUpContainer function for the test.
     The roundtripExportContainer function required for the test needs to be defined separately
     (e.g., by another mixin or the test class itself)
-    This mixin adds the class variable, TABLE_TYPE  which is an int to select between different
+    This mixin adds the class variable, ``TABLE_TYPE``  which is an int to select between different
     container types for testing:
-    TABLE_TYPE=0 : Table of int, float, bool, Enum
-    TABLE_TYPE=1 : Table of int, float, str, bool, Enum
+
+    * ``TABLE_TYPE=0`` : Table of int, float, bool, Enum
+    * ``TABLE_TYPE=1`` : Table of int, float, str, bool, Enum
     """
     TABLE_TYPE = 0
 
@@ -236,8 +310,9 @@ class MixinTestFoo():
     MixinTestZarrToHDF5Foo or MixinTestHDF5ToZarrFoo.
     This mixin adds the class variable, FOO_TYPE  which is an int to select between different
     container types for testing:
-    FOO_TYPE=0 : File with two Foo buckets storing integer datasets
-    FOO_TYPE=1 : File with one Foo buckets storing integer dataset and a SoftLink to it
+
+    * ``FOO_TYPE=0`` : File with two Foo buckets storing integer datasets
+    * ``FOO_TYPE=1`` : File with one Foo buckets storing integer dataset and a SoftLink to it
     """
     FOO_TYPE = 0
     FOO_TYPES = {'int_data': 0,
@@ -275,6 +350,9 @@ class TestHDF5ToZarrDynamicTableC0(MixinTestDynamicTableContainer,
     IGNORE_HDMF_ATTRS = True
     IGNORE_STRING_TO_BYTE = False
     TABLE_TYPE = 0
+
+    def test_simple(self, write_path=None, export_path=None):
+        print(write_path, export_path)
 
 
 class TestZarrToHDF5DynamicTableC0(MixinTestDynamicTableContainer,
