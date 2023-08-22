0d3f9c766b37eedeb58bcd9757c33a250f563ec5
Matthew Avaylon <mavaylon1@berkeley.edu>
2023-08-22 08:22:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ print('found these packages:', pkgs)
 reqs = [
     'hdmf==3.5.4',  # temporary
     'zarr>=2.11.0',
-    'numpy<1.22; python_version=="3.7"',
+    'numpy<1.22; python_version < "3.8"',
+    'numpy>=1.22; python_version >= "3.8",
     'numcodecs>=0.9.1',
     'numcodecs==0.10.2; python_version < "3.8"',
     'numcodecs==0.11.0; python_version >= "3.8"',
