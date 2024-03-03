609c3d95beac977a8299a68cf0d9bf5a2993230f
Matthew Avaylon <mavaylon1@berkeley.edu>
2024-03-03 14:19:23 -0800

    Update test_gallery.py

--- test_gallery.py
+++ test_gallery.py
@@ -73,6 +73,10 @@ _deprecation_warning_pandas_pyarrow_re = (
     r"\nPyarrow will become a required dependency of pandas.*"
 )
 
+_deprecation_warning_datetime = (
+    "datetime.datetime.utcfromtimestamp() is deprecated and scheduled for removal in a future version. "
+    "Use timezone-aware objects to represent datetimes in UTC: datetime.datetime.fromtimestamp(timestamp, datetime.UTC)."
+)
 
 def run_gallery_tests():
     global TOTAL, FAILURES, ERRORS
@@ -134,6 +138,10 @@ def run_gallery_tests():
                 warnings.filterwarnings(
                     # this warning is triggered from pandas
                     "ignore", message=_deprecation_warning_pandas_pyarrow_re, category=DeprecationWarning
+                )
+                warnings.filterwarnings(
+                    # this is triggered from datetime
+                    "ignore", message=_deprecation_warning_datetime, category=DeprecationWarning
                 )
                 _import_from_file(script_abs)
         except Exception:
